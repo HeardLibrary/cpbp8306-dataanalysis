6e0238c9886f55dfa507eefa183d78cf73abd4bc
jborycz <amanofsorrows@gmail.com>
2024-10-03 09:38:53 -0500

    Fix answers

--- S07_20241003_r_data_cleaning_3/main.R
+++ S07_20241003_r_data_cleaning_3/main.R
@@ -1,7 +1,6 @@
 # Data 
 ## ND Gain: https://gain.nd.edu/our-work/country-index
-## Our world in data: https://ourworldindata.org/grapher/co-emissions-per-capita 
-## https://www.heritage.org/index/explore 
+## Our world in data: https://ourworldindata.org/grapher/co-emissions-per-capita
 ## Culture variables: https://github.com/damianruck/Cultural_foundations_of_modern_democracies
 ## Pew data: https://www.pewresearch.org/global/2021/09/14/in-response-to-climate-change-citizens-in-advanced-economies-are-willing-to-alter-how-they-live-and-work/
 ## WVS data: https://www.worldvaluessurvey.org/wvs.jsp
