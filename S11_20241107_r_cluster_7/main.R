40830401f7534dd41b2115657c0ce4b1df8c81d6
jborycz <amanofsorrows@gmail.com>
2024-11-06 12:07:54 -0600

    Add S11

---
+++ S11_20241107_r_cluster_7/main.R
@@ -0,0 +1,19 @@
+## tidymodels
+# https://www.tidymodels.org/
+
+## Libraries
+# install.packages("pacman")
+# library(pacman)
+pacman::p_load(tidyverse, 
+               cluster, 
+               factoextra, 
+               dbscan,
+               mclust,
+               ggdendro,
+               patchwork)
+
+## Load data
+data(iris)
+
+## Scripts
+source("scripts/0_cluster.R")
